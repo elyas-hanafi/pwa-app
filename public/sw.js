ca3bc6fee1d1738b21e04c68a4f3fad378a2ac4a
elyas-hanafi <eliinsta79@gmail.com>
2023-02-28 04:35:01 -0500

    create fetch static

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,15 @@
 console.warn('heyy i am ok');
-
+const staticCacheName = 'site-static';
+const asset = ['/', '/index.html'];
 // install service worker
 this.addEventListener('install', (evt) => {
-  console.log('service worker installed');
+  //   console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(asset);
+    })
+  );
 });
 
 // activate event
