786e16f927d6db2055ecab583744c6d30aa65126
elyas-hanafi <eliinsta79@gmail.com>
2023-02-28 03:20:33 -0500

    update setting

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/OneTimeCode" element={<OneTimeCode />} />
         <Route path="/services" element={<Services />} />
