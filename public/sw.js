10e28f6335d8e3ff8ab565fda23f48bcb80d392e
elyas-hanafi <eliinsta79@gmail.com>
2023-02-28 08:02:30 -0500

    coment service worker

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,29 @@
-console.warn('heyy i am ok');
-const staticCacheName = 'site-static';
-const asset = ['/static/js/bundle.js', '/', '/index.html'];
-// install service worker
-this.addEventListener('install', (evt) => {
-  //   console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(asset);
-    })
-  );
-});
+// console.warn('heyy i am ok');
+// const staticCacheName = 'site-static';
+// const asset = ['/static/js/bundle.js', '/', '/index.html'];
+// // install service worker
+// this.addEventListener('install', (evt) => {
+//   //   console.log('service worker installed');
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       console.log('caching shell assets');
+//       cache.addAll(asset);
+//     })
+//   );
+// });
 
-// activate event
-this.addEventListener('activate', (evt) => {
-  console.log('service worker has been activated');
-});
+// // activate event
+// this.addEventListener('activate', (evt) => {
+//   console.log('service worker has been activated');
+// });
 
-// fetch event
-this.addEventListener('fetch', (evt) => {
-  console.log('fechy fechy');
+// // fetch event
+// this.addEventListener('fetch', (evt) => {
+//   console.log('fechy fechy');
 
-  evt.respondWith(
-    caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
-});
+//   evt.respondWith(
+//     caches.match(evt.request).then((cacheRes) => {
+//       return cacheRes || fetch(evt.request);
+//     })
+//   );
+// });
