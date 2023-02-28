74f464b331e88f35b1a9e510429f4fc1391f723a
elyas-hanafi <eliinsta79@gmail.com>
2023-02-28 03:59:20 -0500

    disable offline mode

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,25 @@
-let cacheData = 'appV1';
-this.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(cacheData).then((cache) => {
-      cache.addAll([
-        '/static/js/main.chunk.js',
-        '/static/js/O.chunk.js',
-        '/static/js/bundle.js',
-        '/index.html',
-        '/',
-      ]);
-    })
-  );
-});
+console.warn('heyy i am ok');
+// let cacheData = 'appV1';
+// this.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(cacheData).then((cache) => {
+//       cache.addAll([
+//         '/static/js/main.chunk.js',
+//         '/static/js/O.chunk.js',
+//         '/static/js/bundle.js',
+//         '/index.html',
+//         '/',
+//       ]);
+//     })
+//   );
+// });
 
-this.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      if (resp) {
-        return resp;
-      }
-    })
-  );
-});
+// this.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((resp) => {
+//       if (resp) {
+//         return resp;
+//       }
+//     })
+//   );
+// });
