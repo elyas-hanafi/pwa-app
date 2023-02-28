d49e869d8982890395366fbb058f1ef147f42d94
elyas-hanafi <eliinsta79@gmail.com>
2023-02-28 03:50:42 -0500

    active service worker

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-// swDev();
+swDev();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
